9593adca61478153a7c93049c26a5df6f29f72f7
Greivin Arias <greivin.babito@gmail.com>
2023-03-13 13:42:56 -0400

    "var created on js"

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,26 @@
 // Assignment code here
-
-
+var symbol = '?.,<>)({}[]=+-/@#!$&*'
+var upperLet = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
+var lowerLet = 'qwertyuiopalksjdhfgmbnvcxz'
+var passLength = 8<128
+var number = '1234567890'
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//Code for our password prompt
+function getPrompts(){
+ var passLength = parseInt(prompt("How long would you like your password to be?")("8-128 characters"))
+ if (isNaN(passLength) || passLength < 8 || passLength > 128){
+  alert("Submit answer between 8 - 128. Numbers only")
+ }
+}
+
+
+  // funcition for generatePassword
+function generatePassword() {
+
+  }
+
 
 // Write password to the #password input
 function writePassword() {
