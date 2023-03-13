42a54947a8cb74ee62a726a7c6757591488fa8cf
Greivin Arias <greivin.babito@gmail.com>
2023-03-13 14:16:14 -0400

    "for choice.concat i needed my var to be an array so i had to convert my prompts into an  array"

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,22 @@
 // Assignment code here
-var symbol = '?.,<>)({}[]=+-/@#!$&*'
-var upperLet = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
-var lowerLet = 'qwertyuiopalksjdhfgmbnvcxz'
+var symbolArray = ['?', '.', ',', '<', '>', ')','(','{','}','[',']','=','+','-','@','#','!','$','*']
+var upperLetArray = ['Q','W','E','R','T','Y','U','I','O','P','L','K','J','H','G','F','D','S','A','Z','X','C','V','B','N','M']
+var lowerLetArray = ['q','w','e','r','t','y','u','i','o','p','a','l','k','s','j','d','h','f','g','m','b','n','v','c','x','z']
 var passLength = 8<128
-var number = '1234567890'
+var numberArray= ['1','2','3','4','5','6','7','8','9','0']
+var choiceArray = []
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 //Code for our password prompt
 function getPrompts(){
- var passLength = parseInt(prompt("How long would you like your password to be?")("8-128 characters"))
+  passLength = parseInt(prompt("How long would you like your password to be?")("8-128 characters"))
  if (isNaN(passLength) || passLength < 8 || passLength > 128){
   alert("Submit answer between 8 - 128. Numbers only")
+  return false;
+ }
+ if (confirm("Would like lower case letters")){
+ choice = choice.concat
  }
 }
 
